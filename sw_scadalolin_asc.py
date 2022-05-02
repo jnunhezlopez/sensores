4f6930e4e483214816b9da5f6b5715dae759abac
Javier Núñez <jnunhezlopez@gmail.com>
2022-05-02 18:06:40 +0200

    MAC de base de datos

--- sw_scadalolin_asc.py
+++ sw_scadalolin_asc.py
@@ -5,8 +5,8 @@ import paho.mqtt.publish as publicador
 import sqlite
 import time
 broker = "192.168.0.18"
-dicSensor = {"CC:50:E3:C7:16:91":1, "84:F3:EB:2F:D1:F0":2, "80:7D:3A:7D:58:4B":3, "60:01:94:14:48:7D":4}
-
+# dicSensor = {"CC:50:E3:C7:16:91":1, "84:F3:EB:2F:D1:F0":2, "80:7D:3A:7D:58:4B":3, "60:01:94:14:48:7D":4}
+dicSensor={}
 def on_connect(client, userdata, flags, rc):
 	print('Conectado (%s)' % client._client_id.decode())
 	client.subscribe(topic='RFID', qos=2)
@@ -22,13 +22,24 @@ def on_message(client, userdata, message):
 		bd = sqlite.conexion("../sensores.db")
 		campos = ["Valor", "IdSensor", "Fecha"]
 		payload =message.payload.rpartition('-')
-		valores= [payload[2], dicSensor[payload[0]], "DateTime('now','localtime')"]
+		try:
+			valorDiccionario=dicSensor[payload[0]]
+		except:
+			print("MAC no registrada, guardando en base de datos")
+			valorDiccionario = bd.insert("sensores",["MAC"],[payload[0]])
+		# valores= [payload[2], dicSensor[payload[0]], "DateTime('now','localtime')"]
+		valores= [payload[2], valorDiccionario, "DateTime('now','localtime')"]
 		bd.insert("Lecturas", campos, valores)
 		bd.cerrarConn()
 	auth = {'username':"SCADA_LOLIN_UP", 'password':"SCADA_LOLIN_UP"}
 	publicador.single("RFID_UP", message.payload, auth=auth, hostname=broker)
 
 def main():
+	# inicialmente se carga el diccionario con los sensores de la base de datos
+	bd = sqlite.conexion("sensores.db")
+	datos = bd.select("sensores")
+	dicSensor = {elemento['MAC']:int(elemento['Id']) for elemento in datos}
+
 	client = suscriptor.Client(client_id='SCADA_LOLIN_UP', clean_session=False)
 	client.username_pw_set('SCADA_LOLIN_UP', 'SCADA_LOLIN_UP')
 	client.on_connect = on_connect
