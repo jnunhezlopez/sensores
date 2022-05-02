6fab90fa5964e419b52e9a1e7829dae29c5c8e93
jnunhezlopez <jnunhezlopez@gmail.com>
2022-05-02 18:53:43 +0200

    MAC fixed Rasp

--- sw_scadalolin_asc.py
+++ sw_scadalolin_asc.py
@@ -6,7 +6,7 @@ import sqlite
 import time
 broker = "192.168.0.18"
 # dicSensor = {"CC:50:E3:C7:16:91":1, "84:F3:EB:2F:D1:F0":2, "80:7D:3A:7D:58:4B":3, "60:01:94:14:48:7D":4}
-dicSensor={}
+
 def on_connect(client, userdata, flags, rc):
 	print('Conectado (%s)' % client._client_id.decode())
 	client.subscribe(topic='RFID', qos=2)
@@ -23,6 +23,7 @@ def on_message(client, userdata, message):
 		campos = ["Valor", "IdSensor", "Fecha"]
 		payload =message.payload.rpartition('-')
 		try:
+			print(dicSensor)
 			valorDiccionario=dicSensor[payload[0]]
 		except:
 			print("MAC no registrada, guardando en base de datos")
@@ -38,8 +39,9 @@ def main():
 	# inicialmente se carga el diccionario con los sensores de la base de datos
 	bd = sqlite.conexion("../sensores.db")
 	datos = bd.select("sensores")
-	dicSensor = {elemento['MAC']:int(elemento['Id']) for elemento in datos}
 
+	global dicSensor 
+	dicSensor= {elemento['MAC']:int(elemento['Id']) for elemento in datos}
 	client = suscriptor.Client(client_id='SCADA_LOLIN_UP', clean_session=False)
 	client.username_pw_set('SCADA_LOLIN_UP', 'SCADA_LOLIN_UP')
 	client.on_connect = on_connect
